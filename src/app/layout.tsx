7364a61765d01e75cdda912755b7b70f25a6c70b
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-07-21 17:51:21 +0200

    feat: integrate wallet connection into main app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default async function RootLayout({
   );
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
