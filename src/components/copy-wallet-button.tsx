dc5d0f7377afda9f6081efb09199d1d4e83e56d6
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-07-22 18:09:18 +0200

    refactor: replace wallet-connected-button with copy-wallet-button

---
+++ src/components/copy-wallet-button.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Check } from "lucide-react";
+import { useCopyToClipboard } from "@/hooks/useCopyToClipboard";
+import { useTruncateAddress } from "@/hooks/useTruncateAddress";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+interface CopyWalletbutton {
+  address: string;
+  ensAvatar?: string;
+  ensName?: string;
+}
+
+export function CopyWalletbutton({
+  address,
+  ensName,
+  ensAvatar: avatar,
+}: CopyWalletbutton) {
+  const { isCopied, copyToClipboard } = useCopyToClipboard();
+  const truncatedAddress = useTruncateAddress(address);
+
+  return (
+    <div className="bg-background text-primary hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 inline-flex w-fit items-center justify-center gap-2 overflow-hidden rounded-full border shadow-xs transition-all">
+      <TooltipProvider key={ensName}>
+        <Tooltip open={isCopied}>
+          <TooltipTrigger asChild>
+            <span
+              onClick={() => copyToClipboard(truncatedAddress)}
+              className="flex h-9 cursor-pointer items-center overflow-hidden px-4 py-2 text-sm font-medium text-ellipsis whitespace-nowrap"
+            >
+              {truncatedAddress}
+            </span>
+          </TooltipTrigger>
+          <TooltipContent>
+            <div className="flex items-center gap-2">
+              <Check className="h-3 w-3" />
+              <span>Address copied!</span>
+            </div>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider key={"ensAvatar"}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Avatar className="h-9 w-9 cursor-default transition-transform duration-200 hover:scale-110 hover:rotate-3">
+              <AvatarImage src={avatar} />
+              <AvatarFallback className="bg-accent">
+                {truncatedAddress.slice(0, 2)}
+              </AvatarFallback>
+            </Avatar>
+          </TooltipTrigger>
+          {ensName && (
+            <TooltipContent>
+              <div className="flex items-center gap-2">
+                <Check className="h-3 w-3" />
+                <span>{ensName}</span>
+              </div>
+            </TooltipContent>
+          )}
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,32 @@
 "use client";
 
-import { Connector, useAccount, useConnect, useDisconnect } from "wagmi";
-import { WalletConnectedButton } from "./wallet-connected-button";
+import {
+  Connector,
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+} from "wagmi";
+import { CopyWalletbutton } from "./copy-wallet-button";
 import { useState } from "react";
 import { WalletConnectModal } from "./wallet-connect-modal";
+import { normalize } from "viem/ens";
+import { LogOut } from "lucide-react";
+import { Button } from "./ui/button";
 
 export function Header() {
   const { disconnect } = useDisconnect();
   const { isConnected, address } = useAccount();
   const { connectors, connect, isPending } = useConnect();
+  const { data: ensName } = useEnsName({
+    address,
+  });
+  const { data: ensAvatar } = useEnsAvatar({
+    name: normalize(ensName!),
+    query: { enabled: !!ensName },
+  });
+
   const [connectingConnector, setConnectingConnector] = useState<string | null>(
     null,
   );
@@ -22,20 +40,27 @@ export function Header() {
     );
   };
 
-  if (!isConnected || !address) {
-    return (
-      <WalletConnectModal
-        connectingConnector={connectingConnector}
-        onConnect={handleConnect}
-        connectors={connectors}
-        isPending={isPending}
-      />
-    );
-  }
   return (
-    <WalletConnectedButton
-      address={address}
-      onDisconnect={() => disconnect()}
-    />
+    <div className="flex items-center justify-end gap-2 p-2">
+      {isConnected && address ? (
+        <>
+          <CopyWalletbutton
+            ensAvatar={ensAvatar ?? undefined}
+            ensName={ensName ?? undefined}
+            address={address}
+          />
+          <Button size="icon" variant="default" onClick={() => disconnect()}>
+            <LogOut />
+          </Button>
+        </>
+      ) : (
+        <WalletConnectModal
+          connectingConnector={connectingConnector}
+          onConnect={handleConnect}
+          connectors={connectors}
+          isPending={isPending}
+        />
+      )}
+    </div>
   );
 }
