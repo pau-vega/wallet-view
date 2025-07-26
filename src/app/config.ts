20bcea48dc1aa65c36520d60c8d73191d58e0b35
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-07-26 09:08:59 +0200

    feat(config): add Polygon support and enable connected chain synchronization

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,10 +1,11 @@
 import { createConfig, http, cookieStorage, createStorage } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import { mainnet, polygon, sepolia } from "wagmi/chains";
 import { walletConnect, metaMask } from "wagmi/connectors";
 
 // Create config once to prevent multiple initializations
 const config = createConfig({
-  chains: [sepolia, mainnet],
+  chains: [sepolia, mainnet, polygon],
+  syncConnectedChain: true,
   ssr: true,
   storage: createStorage({
     storage: cookieStorage,
@@ -18,6 +19,7 @@ const config = createConfig({
   transports: {
     [sepolia.id]: http(),
     [mainnet.id]: http(),
+    [polygon.id]: http(),
   },
 });
 
