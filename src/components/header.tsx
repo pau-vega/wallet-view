ca34e64d45aa369648fe241c21c54a40ff33f706
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-07-26 09:09:25 +0200

    feat(header): enhance header component with wallet icon, title link, and switch chain functionality

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,8 +12,11 @@ import { CopyWalletbutton } from "./copy-wallet-button";
 import { useState } from "react";
 import { WalletConnectModal } from "./wallet-connect-modal";
 import { normalize } from "viem/ens";
-import { LogOut } from "lucide-react";
+import { LogOut, WalletIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import { SwitchChain } from "./switch-chain";
+import { H1 } from "./ui/typography/H1";
+import Link from "next/link";
 
 export function Header() {
   const { disconnect } = useDisconnect();
@@ -41,26 +44,40 @@ export function Header() {
   };
 
   return (
-    <div className="flex items-center justify-end gap-2 p-2">
-      {isConnected && address ? (
-        <>
-          <CopyWalletbutton
-            ensAvatar={ensAvatar ?? undefined}
-            ensName={ensName ?? undefined}
-            address={address}
+    <div className="m-5 flex items-center justify-between gap-2 rounded-md border bg-white p-2 shadow-2xs">
+      <Link href="/">
+        <H1 className="mx-4 flex items-center gap-2 text-xl">
+          Wallet View
+          <WalletIcon className="size-6" />
+        </H1>
+      </Link>
+      <div className="flex items-center gap-2">
+        <SwitchChain />
+        {isConnected && address ? (
+          <>
+            <CopyWalletbutton
+              ensAvatar={ensAvatar ?? undefined}
+              ensName={ensName ?? undefined}
+              address={address}
+            />
+            <Button
+              size="icon"
+              variant="default"
+              onClick={() => disconnect()}
+              title="Disconnect"
+            >
+              <LogOut />
+            </Button>
+          </>
+        ) : (
+          <WalletConnectModal
+            connectingConnector={connectingConnector}
+            onConnect={handleConnect}
+            connectors={connectors}
+            isPending={isPending}
           />
-          <Button size="icon" variant="default" onClick={() => disconnect()}>
-            <LogOut />
-          </Button>
-        </>
-      ) : (
-        <WalletConnectModal
-          connectingConnector={connectingConnector}
-          onConnect={handleConnect}
-          connectors={connectors}
-          isPending={isPending}
-        />
-      )}
+        )}
+      </div>
     </div>
   );
 }
