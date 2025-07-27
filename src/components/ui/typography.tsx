4bb0e1626b030aaf1b1cb52a8f938915f0d862b7
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-07-27 11:09:55 +0200

    fix(typography): update paragraph and inline-code styles for improved readability

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -9,14 +9,14 @@ const typographyVariants = cva("", {
       h2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
       h3: "scroll-m-20 text-2xl font-semibold tracking-tight",
       h4: "scroll-m-20 text-xl font-semibold tracking-tight",
-      p: "leading-7 [&:not(:first-child)]:mt-6",
+      p: "leading-7 [&:not(:first-child)]:mt-6 font-normal sm:text-base",
       large: "text-lg font-semibold",
       lead: "text-muted-foreground text-xl",
       small: "text-sm leading-none font-medium",
       muted: "text-muted-foreground text-sm",
       blockquote: "mt-6 border-l-2 pl-6 italic",
       "inline-code":
-        "bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+        "bg-muted relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm font-normal",
     },
   },
   defaultVariants: {
