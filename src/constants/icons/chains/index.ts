8ed17d89856bf2c4650a1cc54c969ce61adfd514
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-08-18 08:33:16 +0200

    refactor(config): remove Goerli chain from configuration and icon exports

--- src/constants/icons/chains/index.ts
+++ src/constants/icons/chains/index.ts
@@ -1,12 +1,10 @@
-import { goerli, mainnet, polygon, sepolia } from "wagmi/chains";
+import { mainnet, polygon, sepolia } from "wagmi/chains";
 import Ethereum from "../../../../public/icons/chains/ethereum.svg";
 import Polygon from "../../../../public/icons/chains/polygon.svg";
-import Goerli from "../../../../public/icons/chains/goerli.svg";
 import Sepolia from "../../../../public/icons/chains/sepolia.svg";
 
 export const CHAIN_ICONS = {
   [mainnet.id]: Ethereum,
   [polygon.id]: Polygon,
   [sepolia.id]: Sepolia,
-  [goerli.id]: Goerli,
 };
